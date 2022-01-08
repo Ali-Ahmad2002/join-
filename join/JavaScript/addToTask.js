fb8484589e64a924a5f75ad15f6c9adb30243f80
Ali-Ahmad2002 <aliahmad.557755@gmail.com>
2022-01-08 01:42:47 +0100

    task-img-function

--- join/JavaScript/addToTask.js
+++ join/JavaScript/addToTask.js
@@ -2,15 +2,23 @@ setURL('http://gruppe-145.developerakademie.net/smallest_backend_ever');
 
 
 let allTasks = [];
-
+let selectAssignes = [];
 
 async function init() {
     await downloadFromServer();
     allTasks = JSON.parse(backend.getItem('allTasks')) || [];
-
-
+    selectAssignes = JSON.parse(backend.getItem('selectAssignes')) || [];
+    // deleteUser(allTasks);
+    // deleteUser(selectAssignes);
 }
 
+// function deleteUser(allTasks) {
+//     backend.deleteItem('allTasks');
+// }
+
+// function deleteUser(selectAssignes) {
+//     backend.deleteItem('selectAssignes');
+// }
 
 async function addTask() {
     let taskTitle = document.getElementById('titleInputfield').value;
@@ -18,7 +26,7 @@ async function addTask() {
     let taskCategory = document.getElementById('selectCategory').value;
     let taskUrgency = document.getElementById('taskUrgency').value;
     let taskDescription = document.getElementById('taskDescription').value;
-    let taskCreator = document.getElementById('taskCreator');
+    // let taskCreator = document.getElementById('taskCreator');
 
     let task = {
         'taskTitle': taskTitle,
@@ -27,46 +35,48 @@ async function addTask() {
         'taskUrgency': taskUrgency,
         'taskDescription': taskDescription,
         'createdAt': new Date().getTime(),
-        'taskCreator': taskCreator
+        // 'taskCreator': taskCreator,
+        // 'users': selectAssignes
     };
 
     allTasks.push(task);
 
-    // console.log('Hallo', taskTitle);
-    // console.log('Hallo', taskCategory);
-    // console.log('Hallo', taskDate);
-    // console.log('Hallo', taskUrgency);
-    // console.log('Hallo', taskDescription);
-    // console.log('Hallo', task);
-
-    // function changeCreatorImage() {
-    //     let creator = document.getElementById('taskCreator').value;
-    //     let creatorImg = document.getElementById('creatorImg');
-    //     if (creator == 'majd') {
-    //         creatorImg.src = 'join-/join/images/african-lion-ga78658d36_640.jpg';
-    //     }
-    //     if (creator == 'toma') {
-    //         creatorImg.src = './img/Toma.jpeg';
-    //     }
-    //     if (creator == 'peter') {
-    //         creatorImg.src = './img/Peter.jpg';
-    //     }
-    // }
-
-
-
+    console.log(allTasks);
 
     let allTasksAsString = JSON.stringify(allTasks);
     await backend.setItem('allTasks', allTasksAsString);
+
+    // let selectAssignesAsString = JSON.stringify(selectAssignes);
+    // await backend.setItem('selectAssignes', selectAssignesAsString);
 }
 
-function selectImg() {
-    let imgContent = document.getElementById('theImages');
-    document.getElementById('addBtn').classList.add('d-none');
-    document.getElementById('img1').classList.remove('d-none');
-    console.log('hallo')
-}
+let user1 = [
+    {
+        'name': 'ali',
+        'email': 'ali@gmail.com',
+        'img': './images/panda-g17dbddcd4_640.jpg'
+    }
+];
+
+let user2 = [
+    {
+        'name': 'sani',
+        'email': 'sani@gmail.com',
+        'img': './images/african-lion-ga78658d36_640.jpg'
+    }
+]
 
 function addImg(i) {
-
+    if (i == 0) {
+        i = user1;
+        document.getElementById('img2').classList.add('d-none');
+        selectAssignes.push(i);
+    } else if (i == 1) {
+        i = user2;
+        document.getElementById('img1').classList.add('d-none');
+        selectAssignes.push(i);
+    }
+    // console.log(selectAssignes);
+    // console.log('user tasks', allTasks);
 }
+
