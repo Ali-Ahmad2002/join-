7039f7a2eaf7514ddd18d6d55693a3777057b061
SaniSaiti <sani.saiti@bluewin.ch>
2022-02-07 10:08:39 +0100

    login bug fix

--- join/JavaScript/login.js
+++ join/JavaScript/login.js
@@ -18,7 +18,6 @@ async function initi() {
 async function accountregister(event) {
     event.preventDefault();
 
-    //  document.getElementById('loginfailt').classList.add('d-none');
 
     let user = document.getElementById('availableuser').value;
     let pwd = document.getElementById('availablepwd').value;
@@ -26,31 +25,29 @@ async function accountregister(event) {
     console.log(' user', user)
     console.log('pwd', pwd)
 
-    if (pwd == pwd2 && pwd.length > 7 && pwd2.length > 7) {
-        alert('You are now Registered')
-
-
-    } else {
-        alert('passwords do not match or password not equal or too small at least 7 characters')
-        return false;
-        pwd = "";
-        pwd2 = "";
-    }
-
     if (userlogin.find(use => use.username === user)) {
         userexists();
+
     } else {
+        if (pwd == pwd2 && pwd.length >= 7 && pwd2.length >= 7) {
+            alert('You are now Registered');
+
+
+        } else {
+            alert('passwords do not match or password not equal or too small at least 7 characters')
+            return false;
+            pwd = "";
+            pwd2 = "";
+        }
         await pushuser(user, pwd);
-
-
-
-        // document.getElementById('registerfailt').classList.remove('d-none');
-        // document.getElementById('registerfailt').classList.add('registred');
-        // document.getElementById('registerfailt').innerHTML = 'You are now Registered';
 
         setTimeout(() => {
             registered();
         }, 2000);
+
+
+
+
     }
 
     document.getElementById('usersname').value = '';
@@ -86,21 +83,19 @@ async function login() {
 
     } else {
         alert('You are not Registered')
-            // document.getElementById('loginfailt').classList.remove('d-none');
-            // document.getElementById('loginfailt').innerHTML = 'Please enter the right Username or Password<br> or Register please';
-            // document.getElementById('availableuser').value = '';
-            // document.getElementById('availablepwd').value = '';
+
     }
 }
 
 
 function registered() {
-    console.log('Regeistriert');
+    console.log('Usser existiert schon');
+
 }
 
 
 function userexists() {
-    console.log('Usser existiert')
+    alert('Usser existiert')
 }
 
 function load() {
